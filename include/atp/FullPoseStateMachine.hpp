b66d4fa669c9ad856ab7404f8c75e13626c0c96a
Xuhui Zhang <44289986+XHZhang01@users.noreply.github.com>
2023-04-13 11:03:35 +0200

    Update FullPoseStateMachine.hpp

--- include/atp/FullPoseStateMachine.hpp
+++ include/atp/FullPoseStateMachine.hpp
@@ -1,10 +1,3 @@
-/* Copyright (C) Imperial College, Smart Robotics Lab - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by Dimos Tzoumanikas <dt214@ic.ac.uk>
- * 2015-2019
- */
-
 #pragma once
 
 #include <atp/Helper.hpp>
