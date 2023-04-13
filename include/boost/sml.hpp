f2a76d64e1ffd9ff8394a1fbcdb0f1e19097bc30
Xuhui Zhang <44289986+XHZhang01@users.noreply.github.com>
2023-04-13 11:04:19 +0200

    Update sml.hpp

--- include/boost/sml.hpp
+++ include/boost/sml.hpp
@@ -1,9 +1,3 @@
-//
-// Copyright (c) 2016-2019 Kris Jusiak (kris at jusiak dot net)
-//
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
 #ifndef BOOST_SML_HPP
 #define BOOST_SML_HPP
 #if (__cplusplus < 201305L && _MSC_VER < 1900)
